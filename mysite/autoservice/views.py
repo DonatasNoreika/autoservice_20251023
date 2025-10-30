1f78a45c8150fd3e68e8f06adc9e6c44ba64cd28
DonatasNoreika <donoras@gmail.com>
2025-10-30 08:20:58 +0200

    5: +Sukurti puslapį (per klasę views faile), pvz. autoservice/uzsakymai, kuriame būtų atvaizduoti visi serviso užsakymai.

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Service, Car, Order, OrderLine
+from django.views import generic
 
 def index(request):
     context = {
@@ -14,3 +15,8 @@ def cars(request):
 
 def car(request, car_pk):
     return render(request, template_name="car.html", context={"car": Car.objects.get(pk=car_pk)})
+
+class OrderListView(generic.ListView):
+    model = Order
+    template_name = "orders.html"
+    context_object_name = "orders"
