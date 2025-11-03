ba9d676b8736e0e12b6195517920ab62b2a23e90
DonatasNoreika <donoras@gmail.com>
2025-11-03 08:06:26 +0200

    6: +Padaryti, kad įrašai automobilių puslapyje būtų puslapiuojami (per funkciją).

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Service, Car, Order, OrderLine
 from django.views import generic
+from django.core.paginator import Paginator
 
 def index(request):
     context = {
@@ -11,7 +12,11 @@ def index(request):
     return render(request, template_name="index.html", context=context)
 
 def cars(request):
-    return render(request, template_name="cars.html", context={"cars": Car.objects.all()})
+    cars = Car.objects.all()
+    paginator = Paginator(cars, per_page=2)
+    page_number = request.GET.get('page')
+    paged_cars = paginator.get_page(page_number)
+    return render(request, template_name="cars.html", context={"cars": paged_cars})
 
 def car(request, car_pk):
     return render(request, template_name="car.html", context={"car": Car.objects.get(pk=car_pk)})
